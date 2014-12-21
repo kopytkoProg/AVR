eb4985671667f2f13bcf9a65ae3aa9899c9b95f6
kopytkoProg <196858@student.pwr.edu.pl>
2014-12-21 11:36:03 +0100

    Separed code

---
+++ BlinkLedOnUsartDemand/MyBus/MyBus.h
@@ -0,0 +1,56 @@
+/*
+* MyBus.h
+*
+* Created: 2014-12-21 11:04:53
+*  Author: michal
+*/
+
+
+#ifndef MYBUS_H_
+#define MYBUS_H_
+
+/*
+* IncFile1.h
+*
+* Created: 2014-12-20 12:38:36
+*  Author: michal
+*/
+
+//#include <stddef.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+//definiowanie parametrów transmisji za pomoc¹ makr zawartych w pliku
+//nag³ówkowym setbaud.h. Je¿eli wybierzesz prêdkoœæ, która nie bêdzie
+//mo¿liwa do realizacji otrzymasz ostrze¿enie:
+//#warning "Baud rate achieved is higher than allowed"
+
+#define BAUD 9600 //5760													//tutaj podaj ¿¹dan¹ prêdkoœæ transmisji
+#include <util/setbaud.h>													//linkowanie tego pliku musi byæ po zdefiniowaniu BAUD
+
+#define TX_BUFFER_SIZE 32
+#define RX_BUFFER_SIZE 32
+
+// =================== Fields of message ===================
+
+#define MSG_ADDRESS 0
+#define MSG_COMMAND 1
+#define MSG_DATA_LENGTH 2
+
+// =========================================================
+
+volatile unsigned int usart_tx_bufor_ind;									//indeks bufora nadawania
+char usart_tx_bufor[TX_BUFFER_SIZE];										//bufor nadawania
+
+volatile unsigned int usart_rx_bufor_ind;									//indeks bufora nadawania
+char usart_rx_bufor[RX_BUFFER_SIZE];										//bufor nadawania
+
+
+void send_data (char *s, uint8_t length);
+void init_my_buss(void);
+uint8_t crc (char *s, uint8_t length);
+
+
+#endif /* MYBUS_H_ */
